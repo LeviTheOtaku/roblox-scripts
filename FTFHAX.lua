7eb0e03c2a708fdfcea30931429693a88f19e237
Levi <17068563+LeviTheOtaku@users.noreply.github.com>
2023-03-20 14:53:35 +0100

    Update FTFHAX.lua

--- FTFHAX.lua
+++ FTFHAX.lua
@@ -1,4 +1,4 @@
-local ver = "v0.1.7" -- loadstring(game:HttpGet("https://raw.githubusercontent.com/LeviTheOtaku/roblox-scripts/main/FTFHAX.lua",true))()
+local ver = "v0.1.8" -- loadstring(game:HttpGet("https://raw.githubusercontent.com/LeviTheOtaku/roblox-scripts/main/FTFHAX.lua",true))()
 
 local FTFHAX = Instance.new("ScreenGui")
 local MenusTabFrame = Instance.new("Frame")
@@ -918,106 +918,96 @@ spawn(function() -- auto play (buggy and still testing :))
 
 
 local beast = getBeast()
-			local bestpc = getBestPC()[1].pc
-
-			local map = game.ReplicatedStorage.CurrentMap.Value
-			local mapstuff = map:getChildren()
-			for i=1,#mapstuff do
-				if mapstuff[i].Name == "SingleDoor" or mapstuff[i].Name == "DoubleDoor" then
-					local doorParts = mapstuff[i]:getDescendants()
-					for i=1,#doorParts do
-						if doorParts[i].ClassName == "Part" and doorParts[i].Name ~= "Frame" then
-							if not doorParts[i]:findFirstChild("PathfindingModifier") then
-								local a = Instance.new("PathfindingModifier", doorParts[i])
-								a.PassThrough = true
-							end
-						end
-					end
-				end
-			end
-
-			
-			local pcs = getBestPC()
-			local PathfindingService = game:GetService("PathfindingService")
-			local Humanoid = game.Players.LocalPlayer.Character:WaitForChild("Humanoid")
-			local Root = game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart")
-			local goal = nil
-			local agentParams = {
-				AgentRadius = 2,
-				AgentHeight = 2,
-				AgentCanJump = true,
-				AgentWalkableClimb = 4,
-				WaypointSpacing = 2,
-    				Costs = {
-        			avoid = 10.0
-    				}
-			}
+local bestpc = getBestPC()[1].pc
+local map = game.ReplicatedStorage.CurrentMap.Value
+local mapstuff = map:getChildren()
+for i=1,#mapstuff do
+if mapstuff[i].Name == "SingleDoor" or mapstuff[i].Name == "DoubleDoor" then
+local doorParts = mapstuff[i]:getDescendants()
+for i=1,#doorParts do
+if doorParts[i].ClassName == "Part" and doorParts[i].Name ~= "Frame" then
+if not doorParts[i]:findFirstChild("PathfindingModifier") then
+local a = Instance.new("PathfindingModifier", doorParts[i])
+a.PassThrough = true
+end
+end
+end
+end
+end
 
 
-
-			for i, pc in ipairs(pcs) do
-				goal = pc.pc["ComputerTrigger1"].Position
-				local agentParams = {
-					AgentRadius = 2,
-					AgentHeight = 2,
-					AgentCanJump = false,
-					AgentWalkableClimb = 4,
-					WaypointSpacing = 2,
-					Costs = {
-						avoid = 10.0
-					}
-				}
-			
-				local path = PathfindingService:CreatePath(agentParams)
-			
-				path:ComputeAsync(Root.Position, goal)
-				print(path.Status)
-				if path.Status == Enum.PathStatus.Success then
-					local waypoints = path:GetWaypoints()
-					for i, waypoint in ipairs(waypoints) do
-						
-						local currentWaypoint = waypoints[i]
-                local ray = Ray.new(currentWaypoint.Position, Vector3.new(0, 1, 0) * 3)
-                local part = workspace:FindPartOnRay(ray)
-                if part and part.CanCollide then
-                local humanoid = game.Players.LocalPlayer.Character:FindFirstChild("Humanoid")
-                print("need to crouch :)")
-                end
+local pcs = getBestPC()
+local PathfindingService = game:GetService("PathfindingService")
+local Humanoid = game.Players.LocalPlayer.Character:WaitForChild("Humanoid")
+local Root = game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+local goal = nil
+local agentParams = {
+AgentRadius = 2,
+AgentHeight = 2,
+AgentCanJump = true,
+AgentWalkableClimb = 4,
+WaypointSpacing = 2,
+    Costs = {
+        avoid = 10.0
+    }
+}
 
 
 
-					Humanoid:MoveTo(waypoint.Position)
-					local a = Instance.new("Part", workspace)
-					a.Shape = Enum.PartType.Ball
-					a.Position = waypoint.Position
-					a.BrickColor = BrickColor.new("Pink")
-					a.Material = Enum.Material.Neon
-					a.Size = Vector3.new(3,3,3)
-					a.Anchored = true
-					a.CanCollide = false
-					local touch = false
+for i, pc in ipairs(pcs) do
+goal = pc.pc["ComputerTrigger1"].Position
 
-					spawn(function()
-						a.Touched:Connect(function(hit)
-							if hit.Parent:FindFirstChild("Humanoid") then
-							    if hit.Parent.Name == game.Players.LocalPlayer.Character.Name then
-								touch = true
-								a:remove()
-								end
-							end
-						end)
-						wait(10)
-						a:remove()
-					end)
+local path = PathfindingService:CreatePath(agentParams)
 
-					repeat
-						wait(0.05)
-					until touch
-                
-					end
-					break
-				end
-			end
+path:ComputeAsync(Root.Position, goal)
+print(path.Status)
+if path.Status == Enum.PathStatus.Success then
+local waypoints = path:GetWaypoints()
+for i, waypoint in ipairs(waypoints) do
+
+local ray = Ray.new(waypoints[i].Position, Vector3.new(0, 1, 0) * 3)
+local part = workspace:FindPartOnRay(ray)
+if part and part.CanCollide then
+local humanoid = game.Players.LocalPlayer.Character:FindFirstChild("Humanoid")
+print("need to crouch :)")
+end
+
+
+
+Humanoid:MoveTo(waypoint.Position)
+if waypoint.Action == Enum.PathWaypointAction.Jump then
+Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
+end
+    
+local a = Instance.new("Part", workspace)
+a.Shape = Enum.PartType.Ball
+a.Position = waypoint.Position
+a.BrickColor = BrickColor.new("Pink")
+a.Material = Enum.Material.Neon
+a.Size = Vector3.new(3,3,3)
+a.Anchored = true
+a.CanCollide = false
+local touch = false
+
+spawn(function()
+a.Touched:Connect(function(hit)
+if hit.Parent:FindFirstChild("Humanoid") then
+if hit.Parent.Name == game.Players.LocalPlayer.Character.Name then
+touch = true
+a:remove()
+end
+end
+end)
+wait(10)
+a:remove()
+end)
+repeat
+wait(0.05)
+until touch
+end
+break
+end
+end
 				
 				
 				
