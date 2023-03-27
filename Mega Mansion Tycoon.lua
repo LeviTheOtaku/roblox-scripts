4f5144e7eec57829ce4e012fd8ef4a31d7e448d9
Levi <17068563+LeviTheOtaku@users.noreply.github.com>
2023-03-27 09:37:22 +0200

    Update Mega Mansion Tycoon.lua

--- Mega Mansion Tycoon.lua
+++ Mega Mansion Tycoon.lua
@@ -14,7 +14,7 @@ local button = getPlayerTycoon():WaitForChild("Buttons"):getDescendants()
 local cheapest = math.huge
 local cheapestbutton = nil
 for i=1,#button do
-if button[i]:findFirstChild("Head") and button[i]:findFirstChild("Head").Transparency ~= 1 then
+if button[i]:findFirstChild("Head") and button[i]:findFirstChild("Head").Transparency ~= 1 and button[i]:findFirstChild("Price") then
 if button[i].Price.Value < cheapest then
 cheapest = button[i].Price.Value
 cheapestbutton = button[i]
@@ -23,8 +23,8 @@ end
 end
 local button2 = workspace:WaitForChild("ClientButtons"):getDescendants()
 for i=1,#button2 do
-if button2[i]:findFirstChild("Head") and button2[i]:findFirstChild("Head").Transparency ~= 1 then
-if button2[i].Price.Value < cheapest then -- doesn't work in penthouse since they dont store price as variable
+if button2[i]:findFirstChild("Head") and button2[i]:findFirstChild("Head").Transparency ~= 1 and button2[i]:findFirstChild("Price") then
+if button2[i].Price.Value < cheapest then
 cheapest = button2[i].Price.Value
 cheapestbutton = button2[i]
 end
@@ -50,12 +50,30 @@ if findMailbox[i].Name == "Giver" then
 mailbox = findMailbox[i]
 end
 end
+if mailbox ~= nil then
 local character = game:GetService("Players").LocalPlayer.Character or game:GetService("Players").LocalPlayer.CharacterAdded:Wait()
 local charparts = character:FindFirstChildWhichIsA("BasePart")
 firetouchinterest(mailbox,charparts,0)
 wait()
 firetouchinterest(mailbox,charparts,1)
+else
+local screen = nil
+local findScreen = workspace["ClientApartmentObjects"]["Level3Start"].phStart:getDescendants()
+for i=1,#findScreen do
+if findScreen[i].Name == "CollectGui" then
+screen = findScreen[i].Parent
 end
+end
+if screen ~= nil then
+local character = game:GetService("Players").LocalPlayer.Character or game:GetService("Players").LocalPlayer.CharacterAdded:Wait()
+local charparts = character:FindFirstChildWhichIsA("BasePart")
+firetouchinterest(screen,charparts,0)
+wait()
+firetouchinterest(screen,charparts,1)
+end
+end
+end
+
 
 while true do
 wait(0.5)
